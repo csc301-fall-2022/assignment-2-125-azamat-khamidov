5cd5f5b8219a35c755a2118d598a160cdac00400
Azamat Khamidov <azamat.khamidov@mail.utoronto.ca>
2022-10-18 22:28:00 -0400

    Testing git actions with xcode files

---
+++ checkout-calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  checkout-calculator
+//
+//  Created by Azamat Khamidov on 2022-10-18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
