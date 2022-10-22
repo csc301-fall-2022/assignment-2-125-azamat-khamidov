7eadbea936e46b0a57e0fb9182af79394702a68d
Azamat Khamidov <azamat.khamidov@mail.utoronto.ca>
2022-10-21 22:27:37 -0400

    Initialize Swift Package Manager

---
+++ Sources/assignment-2-125-azamat-khamidov/assignment_2_125_azamat_khamidov.swift
@@ -0,0 +1,6 @@
+public struct assignment_2_125_azamat_khamidov {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/assignment-2-125-azamat-khamidovTests/assignment_2_125_azamat_khamidovTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import assignment_2_125_azamat_khamidov
+
+final class assignment_2_125_azamat_khamidovTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(assignment_2_125_azamat_khamidov().text, "Hello, World!")
+    }
+}
